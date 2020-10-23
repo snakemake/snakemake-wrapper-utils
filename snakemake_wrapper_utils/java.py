a311d986469279af56104a8467253d3f232afcb0
David Laehnemann <david.laehnemann@hhu.de>
2020-10-23 16:29:52 +0200

    Remove suggestion of mem_gb for now.

--- snakemake_wrapper_utils/java.py
+++ snakemake_wrapper_utils/java.py
@@ -8,18 +8,18 @@ def get_java_opts(snakemake):
     # By doing so, backward compatibility is preserved
     if "mem_mb" in snakemake.resources.keys():
         if "-Xmx" in java_opts:
-            sys.exit("You have specified resources.mem_mb and provided `-Xmx` in params.java_opts. For Java memory specifications, please only use resources.mem_mb or resources.mem_gb in the future.")
+            sys.exit("You have specified resources.mem_mb and provided `-Xmx` in params.java_opts. For Java memory specifications, please only use resources.mem_mb.")
         if "-Xmx" in extra:
-            sys.exit("You have specified resources.mem_mb and provided `-Xmx` in params.extra. For Java memory specifications, please only use resources.mem_mb or resources.mem_gb in the future.")
+            sys.exit("You have specified resources.mem_mb and provided `-Xmx` in params.extra. For Java memory specifications, please only use resources.mem_mb.")
         java_opts += " -Xmx{}M".format(snakemake.resources["mem_mb"])
 
     # Getting memory in gigabytes, for user convenience. Please prefer the use
     # of mem_mb over mem_gb as advised in documentation.
     elif "mem_gb" in snakemake.resources.keys():
         if "-Xmx" in java_opts:
-            sys.exit("You have specified resources.mem_mb and provided `-Xmx` in params.java_opts. For Java memory specifications, please only use resources.mem_mb or resources.mem_gb in the future.")
+            sys.exit("You have specified resources.mem_gb and provided `-Xmx` in params.java_opts. For Java memory specifications, please only use resources.mem_mb.")
         if "-Xmx" in extra:
-            sys.exit("You have specified resources.mem_mb and provided `-Xmx` in params.extra. For Java memory specifications, please only use resources.mem_mb or resources.mem_gb in the future.")
+            sys.exit("You have specified resources.mem_gb and provided `-Xmx` in params.extra. For Java memory specifications, please only use resources.mem_mb.")
         java_opts += " -Xmx{}G".format(snakemake.resources["mem_gb"])
 
     # Getting java temp directory from output files list, if -Djava.io.tmpdir
