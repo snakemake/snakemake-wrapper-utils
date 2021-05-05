d8e1ce2a34db8e04f24ba45e88bfca65a71d8b1a
fgvieira <fgarrettvieira@gmail.com>
2021-05-05 10:41:54 +0200

    Added write_index parsing

--- snakemake_wrapper_utils/samtools.py
+++ snakemake_wrapper_utils/samtools.py
@@ -2,10 +2,17 @@ import sys
 from os import path
 
 
-def get_samtools_opts(snakemake, parse_threads=True, parse_output_format=True):
+def get_samtools_opts(
+    snakemake,
+    parse_threads=True,
+    parse_write_index=True,
+    parse_output=True,
+    parse_output_format=True,
+):
     """Obtain samtools_opts from output, params, and handle resource definitions in resources."""
     samtools_opts = ""
     extra = snakemake.params.get("extra", "")
+    idx = snakemake.output.get("idx", "")
 
     ###############
     ### Threads ###
@@ -18,8 +25,31 @@ def get_samtools_opts(snakemake, parse_threads=True, parse_output_format=True):
             samtools_opts += (
                 ""
                 if snakemake.threads <= 1
-                else "--threads {}".format(snakemake.threads - 1)
+                else " --threads {}".format(snakemake.threads - 1)
             )
+
+    ###################
+    ### Write index ###
+    ###################
+    if parse_write_index:
+        if "--write-index" in extra:
+            sys.exit(
+                "You have specified writing index (`--write-index`) in params.extra; this is automatically infered from `idx` output file."
+            )
+            if idx:
+                samtools_opts += " --write-index"
+
+    ###################
+    ### Output file ###
+    ###################
+    if parse_output:
+        if "-o" in extra:
+            sys.exit(
+                "You have specified output file (`-o`) in params.extra; this is automatically infered from the first output file."
+            )
+            samtools_opts += " -o {snakemake.output[0]}"
+            if idx:
+                samtools_opts += f"##idx##{idx}"
 
     #####################
     ### Output format ###
@@ -29,7 +59,6 @@ def get_samtools_opts(snakemake, parse_threads=True, parse_output_format=True):
             sys.exit(
                 "You have specified output format (`-O/--output-fmt`) in params.extra; this is automatically infered from output file extension."
             )
-
         out_name, out_ext = path.splitext(snakemake.output[0])
         out_ext = out_ext[1:].upper()
         samtools_opts += f" --output-fmt {out_ext}"
