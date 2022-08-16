e916c8b56600655798ff7c1e12133aa44000035c
tdayris <thibault.dayris@outlook.fr>
2022-08-16 14:28:44 +0200

    feat: Regions, samples and targets in get_bcftools_opts (#19)
    * new input files added
    
    * black formatting
    
    Co-authored-by: tdayris <tdayris@gustaveroussy.fr>

--- snakemake_wrapper_utils/bcftools.py
+++ snakemake_wrapper_utils/bcftools.py
@@ -19,6 +19,9 @@ def get_bcftools_opts(
     snakemake,
     parse_threads=True,
     parse_ref=True,
+    parse_region=True,
+    parse_samples=True,
+    parse_targets=True,
     parse_output=True,
     parse_output_format=True,
     parse_memory=True,
@@ -26,7 +29,6 @@ def get_bcftools_opts(
     """Obtain bcftools_opts from output, params, and handle resource definitions."""
     bcftools_opts = ""
     extra = snakemake.params.get("extra", "")
-
 
     ###############
     ### Threads ###
@@ -42,7 +44,6 @@ def get_bcftools_opts(
             else "--threads {}".format(snakemake.threads - 1)
         )
 
-
     ######################
     ### Reference file ###
     ######################
@@ -55,6 +56,41 @@ def get_bcftools_opts(
         if snakemake.input.get("ref"):
             bcftools_opts += f" --fasta-ref {snakemake.input.ref}"
 
+    ####################
+    ### Regions file ###
+    ####################
+    if parse_region:
+        if "--region-file" in extra or "-R" in extra:
+            sys.exit(
+                "You have specified region file (`-R/--regions-file`) in `params.extra`; this is automatically infered from the `regions` input file."
+            )
+
+        if snakemake.input.get("regions"):
+            bcftools_opts += f" --regions-file {snakemake.input.regions}"
+
+    ####################
+    ### Samples file ###
+    ####################
+    if parse_samples:
+        if "-S" in extra or "--samples-file" in extra:
+            sys.exit(
+                "You have specified samples file (`-S/--samples-file`) in `params.extra`; this is automatically infered from the `samples` input file."
+            )
+
+        if snakemake.input.get("samples"):
+            bcftools_opts += f" --samples-file {snakemake.input.samples}"
+
+    ####################
+    ### Targets file ###
+    ####################
+    if parse_targets:
+        if "-T" in extra or "--targets-file" in extra:
+            sys.exit(
+                "You have specified samples file (`-T/--targets-file`) in `params.extra`; this is automatically infered from the `targets` input file."
+            )
+
+        if snakemake.input.get("targets"):
+            bcftools_opts += f" --targets-file {snakemake.input.targets}"
 
     ###################
     ### Output file ###
@@ -65,7 +101,6 @@ def get_bcftools_opts(
                 "You have specified output file (`-o/--output`) in `params.extra`; this is automatically infered from the first output file."
             )
         bcftools_opts += f" -o {snakemake.output[0]}"
-
 
     #####################
     ### Output format ###
@@ -80,7 +115,6 @@ def get_bcftools_opts(
             snakemake.output[0], snakemake.params.get("uncompressed_bcf", False)
         )
         bcftools_opts += f" --output-type {out_format}"
-
 
     ##############
     ### Memory ###
@@ -98,7 +132,6 @@ def get_bcftools_opts(
         elif "mem_gb" in snakemake.resources.keys():
             bcftools_opts += " --max-mem {}G".format(snakemake.resources["mem_gb"])
 
-
     ################
     ### Temp dir ###
     ################
@@ -106,6 +139,5 @@ def get_bcftools_opts(
         sys.exit(
             "You have provided `-T/--temp-dir/--temp-prefix` in `params.extra`; please use `resources.tmpdir`."
         )
-
 
     return bcftools_opts
