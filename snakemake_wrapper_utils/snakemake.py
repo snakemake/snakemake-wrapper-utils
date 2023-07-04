bb473410f7af40bb4ad98cf5006c7d6302295f4a
Christopher Schröder <christopher.schroeder@tu-dortmund.de>
2023-07-04 09:23:09 +0000

    gb in mb

--- snakemake_wrapper_utils/snakemake.py
+++ snakemake_wrapper_utils/snakemake.py
@@ -1,11 +1,15 @@
 def get_mem(snakemake, out_unit="MiB"):
     """
     Obtain requested memory (from resources) and return in given units.
-    If no memory resources found, return a value equivalent to 205 MiB.
+    If no memory resources found, return a value equivalent to 205 .
     """
 
     # Store memory in MiB
-    mem_mb = snakemake.resources.get("mem_gb", snakemake.resources.get("mem_mb", 205))
+
+    if mem_mb := snakemake.resources.get("mem_gb", None):
+        mem_mb *= 1024
+    else:
+        mem_mb = snakemake.resources.get("mem_mb", 205)
 
     if out_unit == "KiB":
         return mem_mb * 1024
