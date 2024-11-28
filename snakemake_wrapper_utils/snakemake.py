2d372a4efe76717660ef428d1da5d0e8bb0d495a
Filipe G. Vieira <1151762+fgvieira@users.noreply.github.com>
2024-11-29 00:15:04 +0100

    Parse file format from extension (ignore compression)

--- snakemake_wrapper_utils/snakemake.py
+++ snakemake_wrapper_utils/snakemake.py
@@ -1,7 +1,7 @@
 def get_mem(snakemake, out_unit="MiB"):
     """
     Obtain requested memory (from resources) and return in given units.
-    If no memory resources found, return a value equivalent to 205 .
+    If no memory resources found, return a value equivalent to 205.
     """
 
     # Store memory in MiB
@@ -22,8 +22,22 @@ def get_mem(snakemake, out_unit="MiB"):
 
 
 def is_arg(arg, cmd):
-    """Check command for the presence of argument"""
-    if arg in cmd.replace("=", " ").split(" "):
-        return True
+    """Check command for the presence of argument."""
+    return arg in cmd.replace("=", " ").split(" "):
 
-    return False
+
+def get_format(path):
+    """Get file format from extension, ignoring common compressions."""
+    from pathlib import Path
+    exts = Path(path).suffixes
+    if exts[-1] in (".gz", ".bgz", ".bz2"):
+        ext = exts[-2]
+    else:
+        ext = exts[-1]
+
+    if ext in (".fq", ".fastq"):
+        return "fastq"
+    elif ext in (".fa", ".fas", ".fna", ".fasta"):
+        return "fasta"
+    else:
+        return ext.lower()
