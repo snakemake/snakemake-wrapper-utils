360759677cc7b8ec3b47c253bc6090d6e7f29803
Filipe G. Vieira <1151762+fgvieira@users.noreply.github.com>
2025-03-21 12:55:48 +0100

    Add space

--- snakemake_wrapper_utils/gatk.py
+++ snakemake_wrapper_utils/gatk.py
@@ -34,7 +34,7 @@ def get_gatk_opts(
             if isinstance(arg_file, list):
                 arg_file = " --argument_file ".join(arg_file)
 
-            gatk_opts += f"--argument_file {arg_file}"
+            gatk_opts += f" --argument_file {arg_file}"
 
     ######################
     ### Reference file ###
