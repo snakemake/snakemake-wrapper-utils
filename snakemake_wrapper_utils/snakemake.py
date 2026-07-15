11a39b5b507dffc0b3035c379ea7d6e70942639b
Filipe G. Vieira <1151762+fgvieira@users.noreply.github.com>
2026-07-15 15:02:35 +0200

    Fix logic

--- snakemake_wrapper_utils/snakemake.py
+++ snakemake_wrapper_utils/snakemake.py
@@ -132,10 +132,12 @@ def move_files(snakemake, mapping, cmd="mv -v", required=True):
     cmds = []
     for out_tag, tool_out_name in mapping.items():
         out_name = snakemake.output.get(out_tag, "")
-        if required and not out_name:
-            raise KeyError(
-                f"The wrapper requires the named output: {out_tag}. Please provide this named output."
-            )
-        cmds.append(f"{cmd} '{tool_out_name}' '{out_name}'")
+        if out_name:
+            cmds.append(f"{cmd} '{tool_out_name}' '{out_name}'")
+        else:
+            if required:
+                raise KeyError(
+                    f"The wrapper requires the named output: {out_tag}. Please provide this named output."
+                )
 
     return cmds
